df5b3de1133b31c076a379c3d48e6cd3fb2ce4fc
Selim <mselimyaman@gmail.com>
2022-07-19 12:26:03 +0300

    keeperApp Pt2 Completed

---
+++ src/components/Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+function createNote(note){
+  return (
+    <Note 
+      key = {note.key}
+      title = {note.title}
+      content = {note.content}
+    />
+  );
+}
+export default createNote;
